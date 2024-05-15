3570c97717767b855dcde93a2847080b3a6395af
Andres-Lozano <wilsonandres135@gmail.com>
2024-05-15 16:08:31 -0500

    comentarios

--- EjemplosArreglos/EjercicioArreglosPOO/src/paquete02/Ejecutor2.java
+++ EjemplosArreglos/EjercicioArreglosPOO/src/paquete02/Ejecutor2.java
@@ -1,13 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package paquete02;
 
-/**
- *
- * @author Usuario iTC
- */
+import paquete01.LibretaCalificacion;
+
 public class Ejecutor2 {
+    public static void main(String[]arg ){
+        double[] notas = {10,9,10};
+        String nombreEstudiante = "Rene Elizalde";
+        
+        LibretaCalificacion libreta = new LibretaCalificacion(nombreEstudiante,
+        notas);
+       // libreta.establecerPromedio();
+        //libreta.establecerPromedioCualitativo();
+        System.out.printf("%s\n", libreta);
+            
+        
+    }
     
 }
